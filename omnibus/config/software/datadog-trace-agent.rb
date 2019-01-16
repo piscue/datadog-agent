94d5cfc5d7c71f84a14d8dd9da756618fe505d99
Derek Brown <derek.brown@datadoghq.com>
2019-01-16 10:56:42 -0800

    Fix trace agent software def to build in pipeline

--- omnibus/config/software/datadog-trace-agent.rb
+++ omnibus/config/software/datadog-trace-agent.rb
@@ -7,31 +7,12 @@ require "./lib/ostools.rb"
 require 'pathname'
 
 name "datadog-trace-agent"
-
-trace_agent_version = ENV['TRACE_AGENT_VERSION']
-if trace_agent_version.nil? || trace_agent_version.empty?
-  trace_agent_version = 'master'
-end
-
-default_version trace_agent_version
-
-source path: '..'
 relative_path 'src/github.com/DataDog/datadog-agent'
 
 build do
   # set GOPATH on the omnibus source dir for this software
   gopath = Pathname.new(project_dir) + '../../../..'
   if windows?
-    env = {
-      # Trace agent uses GNU make to build.  Some of the input to gnu make
-      # needs the path with `\` as separators, some needs `/`.  Provide both,
-      # and let the makefile sort it out (ugh)
-
-      # also on windows don't modify the path.  Modifying the path here mixes
-      # `/` with `\` in the PATH variable, which confuses the make (and sub-processes)
-      # below.  When properly configured the path on the windows box is sufficient.
-      'GOPATH' => "#{windows_safe_path(gopath.to_path)}",
-    }
   else
     env = {
       'GOPATH' => gopath.to_path,
